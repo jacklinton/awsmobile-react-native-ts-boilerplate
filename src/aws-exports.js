c6cb57ae43896289f8c52f04846d826ce5006201
Jack Linton <jacklinton@mac.com>
2018-02-07 00:14:48 -0500

    Create aws-exports config object.

---
+++ src/aws-exports.js
@@ -0,0 +1,6 @@
+const config = {
+    identityPoolId: 'us-east-1:b236d6a5-d57b-4a3c-9f51-3b579c791a87',
+    region: 'us-east-1',
+    userPoolId: 'us-east-1_ziwLRSjt9',
+    userPoolWebClientId: '73nld7d7i1861ok3fsnmhtpfhv'
+};
